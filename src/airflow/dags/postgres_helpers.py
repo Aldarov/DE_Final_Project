1330eb5dbb67dc4610a6a76f03e8e9cfbb426d20
a.aldarov <alex_aldarov@mail.ru>
2022-12-20 17:02:25 +0800

    fix

--- src/airflow/dags/postgres_helpers.py
+++ src/airflow/dags/postgres_helpers.py
@@ -1,8 +1,15 @@
 import psycopg2
 import traceback
+import os
 from log_service import add_error
-from airflow.models import Variable
-from airflow.hooks.base_hook import BaseHook
+from dotenv import load_dotenv, dotenv_values
+
+class connection:
+    host: str
+    port: str
+    database: str
+    login: str
+    password: str
 
 def get_connection():
     """Возвращает объект с параметрами соединения к БД
@@ -17,15 +24,17 @@ def get_connection():
                     password: str        пароль
                 }
     """
-    conn_name = Variable.get("conn_name")
-    connection = BaseHook.get_connection(conn_name)
+    load_dotenv(os.path.join(os.path.dirname(__file__), '../../../.env'))
 
-    # class connection:
-    #     host: str = "localhost"
-    #     port: str = "5432"
-    #     schema: str = "news_analysis"
-    #     login: str = "postgres"
-    #     password: str = "postgres"
+    # SECRET_KEY = os.environ.get("SECRET_KEY")
+
+    connection.host = os.environ.get('POSTGRES_HOST')
+    connection.port = os.environ.get('POSTGRES_PORT')
+    connection.database = os.environ.get('POSTGRES_DB_NAME')
+    connection.login = os.environ.get('POSTGRES_USER')
+    connection.password = os.environ.get('POSTGRES_PASSWORD')
+
+    print("conn: ", dotenv_values('.env')["POSTGRES_HOST"], connection.host, connection.port, connection.database, connection.login, connection.password)
 
     return connection
 
@@ -37,7 +46,7 @@ def runs_sql_queries(action):
             any -- результат выполнения функция action
     """
     connection = get_connection()
-    conn = psycopg2.connect(host=connection.host, port=connection.port, user=connection.login, password=connection.password, database=connection.schema)
+    conn = psycopg2.connect(host=connection.host, port=connection.port, user=connection.login, password=connection.password, database=connection.database)
     try:
         with conn:
             with conn.cursor() as cursor:
